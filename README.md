48a53b9391f6041815e836bf85de5090e7e2df31
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-07-31 23:15:24 +0200

    adding ci

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # E-Robots
+[![Build Status](https://travis-ci.org/meox/e-robots.svg?branch=master)](https://travis-ci.org/meox/e-robots)
 
 Revival of:
 http://tpoindex.github.io/crobots/docs/crobots_manual.html
--- lib/lang/asm_gen.ex
+++ lib/lang/asm_gen.ex
@@ -2,16 +2,16 @@ defmodule ASM.Generator do
   alias ASM.GenCTX
 
   def generate(vm_code) do
-    gen(vm_code, %GenCTX{})
+    #gen(vm_code, %GenCTX{})
   end
 
   defp gen([op, ops], ctx = %GenCTX{}) do
-    c = decode(op, ctx)
-    gen(ops, %{ctx | code: Map.put(ctx.code, c)})
+    #c = decode(op, ctx)
+    #gen(ops, %{ctx | code: Map.put(ctx.code, c)})
   end
 
   ###
 
-  defp decode({:op}) do
-  end
+  #defp decode({:op}) do
+  #end
 end
